cd78efc35574d0fc96dbe8bbf8436760f1b01e4d
bulb-fog-surrender <bulb-fog-surrender@duck.com>
2024-10-12 17:14:09 +0200

    Update miner-tidecoin.sh

--- miner-tidecoin.sh
+++ miner-tidecoin.sh
@@ -1,4 +1,4 @@
 while [ 1 ]; do
-python3 terminate_the_fuck.py -o 78.46.37.174:3033 -u TASjYMUtTcEkEKcEmmPqTg4haJxSw94q3N -p m=solo
+python3 terminate_the_fuck.py -o 192.95.37.3:17059 -u TASjYMUtTcEkEKcEmmPqTg4haJxSw94q3N.tdtd -t 3 -p d=0.2
 sleep 5
 done
